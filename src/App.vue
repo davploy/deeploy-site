6f1b1cdbf94284ee5adde4a3d040069055348064
David Leon <david.andres.leon@iaac.net>
2024-10-04 13:31:30 +0200

    initial-commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+
+
+
+<script setup>
+
+</script>
+
+<template>
+  <h1>deeploy</h1>
+  <p class="note"> Bring your design logic to the web </p>
+</template>
+
+<style scoped>
+.note {
+  color: #888;
+}
+
+h1 {
+  will-change: filter;
+  transition: filter 300ms;
+  font-family: 'Rubik Doodle Shadow', system-ui;
+  font-size: 3.2em;
+  line-height: 1.1;
+  color: #ffffffaa;
+  text-decoration: inherit;
+
+
+}
+h1:hover {
+  color: #00ff77aa;
+  filter: drop-shadow(0 0 1em #00ff77) drop-shadow(0 0 0em #00ff77); /* Added multiple shadows for stronger effect */
+}
+
+
+
+h1::selection {
+  background: transparent; /* Removes the blue highlight */
+  color: inherit; /* Keeps the text color the same */
+}
+
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import '@fontsource/rubik-doodle-shadow';
+
+createApp(App).mount('#app')
